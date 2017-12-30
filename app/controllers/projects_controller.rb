fda885dc38398ced8a965dca411a72e3c9a9acbd
Jordan Hudgens <jordan@wow.com>
2017-12-30 13:04:27 -0700

    Integrated knock into the projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,17 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user
   before_action :set_project, only: [:show, :update, :destroy]
 
-  # GET /projects
   def index
     @projects = Project.all
 
     render json: @projects
   end
 
-  # GET /projects/1
   def show
     render json: @project
   end
 
-  # POST /projects
   def create
     @project = Project.new(project_params)
 
@@ -24,7 +22,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
   def update
     if @project.update(project_params)
       render json: @project
@@ -33,19 +30,20 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
   def destroy
     @project.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def project_params
-      params.require(:project).permit(:user_id, :name, :application_summary)
+      params.require(:project).permit(
+        :user_id,
+        :name,
+        :application_summary
+      )
     end
 end
